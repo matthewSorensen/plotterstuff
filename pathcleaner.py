e7fa3b49e23d335c70643efd6554a41fc3405084
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2020-10-09 01:09:06 -0700

    delete duplicates

--- pathcleaner.py
+++ pathcleaner.py
@@ -1,6 +1,31 @@
 from scipy.spatial import KDTree
 import numpy as np
 
+# stages:
+# throw out duplicates
+# link paths (also reorder for merge)
+# merge
+# reduce segments
+
+def clean_paths(paths, link = True, reverse = True, deduplicate = True, merge = True, reduce = None):
+
+    if deduplicate:
+        paths = list(remove_duplicates(paths))
+
+    if link:
+        paths = link_paths(paths, reverse = reverse)
+
+    if merge is not None:
+        paths = merge_paths(paths, merge)
+
+    if reduce is not None:
+
+        paths = (reduce_points(iter(p), reduce) for p in paths)
+
+    return list(paths)
+    
+
+
 # We are highly lazy and pragmatic, and are gonna do something stupid with pretending scikit's
 # KDTrees are dynamic...
 def link_paths(paths, reverse = True, k = 20, log_rebuilds = False):
@@ -83,4 +108,105 @@ def merge_paths(paths, staydown):
             prev = [p]
             
     yield np.concatenate(prev)
+    
+def polyline_length(line):
+    acc = 0
+    n, _ = line.shape
+    for i in range(1,n):
+        delta = line[i] - line[i - 1]
+        acc += np.sqrt(delta.dot(delta))
+        
+    return acc
 
+def pointwise_equal(a,b, epsilon):
+    if len(a) != len(b):
+        return False
+    return np.all(np.abs(a - b) < epsilon)
+
+def find_runs(lengths, epsilon):
+    idx = np.argsort(lengths)
+    start_i = idx[0]
+    start = lengths[start_i]
+    acc = [start_i]
+    
+    for i in idx[1:]:
+        length = lengths[i]
+        if length - start < epsilon:
+            acc.append(i)
+        else:
+            if len(acc) > 1:
+                yield acc
+            acc = [i]
+            start = length
+            
+    if len(acc) > 1:
+        yield acc
+        
+def tree_search_endpoints(paths, group, epsilon):
+    n = len(group)
+    points = np.empty((n,4))
+    # Put all of the paths into a kd-tree as 4d points
+    # representing the box spanned by their endpoints
+    for i, g in enumerate(group):
+        p = paths[g]
+        (a,b),(c,d) = p[0], p[-1]    
+        points[i] = min(a,c), max(a,c), min(b,d), max(b,d)
+    live = np.ones(n, dtype = int)
+    tree = KDTree(points)
+    
+    for i in range(n):
+        if not live[i]:
+            continue
+        results = tree.query_ball_point(points[i],epsilon)
+        if len(results) < 2:
+            continue
+        # Check each close point - if they're equal
+        # (max norm pointwise), it's a duplicate and thus must die...
+        for j in results:
+            if j <= i:
+                continue
+            pi, pj = paths[group[i]], paths[group[j]]
+            if pointwise_equal(pi,pj, epsilon):
+                live[j] = 0
+                
+    return live
+
+
+def remove_duplicates(paths, epsilon = 1e-6):
+    n = len(paths)
+    keep = np.ones(n, dtype = int)
+    lengths = np.empty(n)
+    for i in range(n):
+        lengths[i] = polyline_length(paths[i])
+    
+    for group in find_runs(lengths, epsilon):
+        if len(group) == 2:
+            i,j = group
+            if pointwise_equal(paths[i],paths[j],epsilon):
+                keep[j] = 0
+            continue
+    
+        for i, v in enumerate(tree_search_endpoints(paths, group, epsilon)):
+            keep[group[i]] = v
+            
+    for i,v in enumerate(keep):
+        if v:
+            yield paths[i]
+
+def reduce_points(pts, ball):
+    """ Preserves start and end, but reduces small segments. No
+    great theoretical basis and it preserves nothing else - length etc.."""
+    last = next(pts)
+    segment = [last]
+    ball = ball ** 2
+    
+    for pt in pts:
+        delta = last - pt
+        if ball <= delta.dot(delta):
+            segment.append(pt)
+            last = pt
+            
+    if delta.dot(delta) > 1e-18:
+        segment.append(pt)
+        
+    return np.array(segment)
