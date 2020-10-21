e00e1798b66a8edf22324a0b11219eaea8e50522
Matthew Sorensen <matthew.d.sorensen@gmail.com>
2020-10-20 18:55:42 -0700

    this is kinda ok?

---
+++ burin.py
@@ -0,0 +1,182 @@
+#!/usr/bin/env python3
+import click
+import json
+import os
+import shutil
+
+import pathcleaner
+import dxfloader
+import processes
+
+def get_blob(directory):
+    blob = os.path.join(directory,"state.json")
+    if not os.path.exists(blob):
+        print(f"Unable to find workflow state {blob}")
+        exit(-1)
+    with open(blob,"r") as f:
+        return json.load(f)
+    
+def save_blob(directory, blob):
+    # Make sure that the output directory is actually a thing
+    if not os.path.exists(directory):
+        os.mkdir(directory)
+    elif not os.path.isdir(directory):
+        print(f"Output directory '{directory}' already exists, but is not a directory")
+        exit(-1)
+        
+    blob_path = os.path.join(directory,"state.json")
+    with open(blob_path,'w') as f:
+        json.dump(blob, f)
+        
+    return blob_path
+
+    
+def check_parameters(blob, unit,prompt = True, reset = False):
+
+    stages, parameters, units = blob['stage'], blob['parameters'], blob['units']
+    ok,updated = True,False
+
+    # If we're resetting parameters for this layer, purge the old ones
+    if reset and (unit in parameters):
+        parameters[unit] = {}
+        updated = True
+    # Go through all of the previous units (+ the current) and make
+    # sure all of the required parameters are there
+    for i in range(0, 1 + stages[unit]):
+        u = units[i]
+        name = u['name']
+        if name not in parameters:
+            parameters[name] = {}
+        scope = parameters[name]
+        for p in u['parameters']:
+            if p not in scope:
+                if prompt:
+                    scope[p] = input(f"Parameter {p} for unit {name}: ").strip()
+                    updated = True
+                else:
+                    ok = False
+                    
+    return ok, updated
+
+        
+@click.group()
+@click.pass_context
+def main(ctx):    
+    pass
+
+@main.command()
+@click.argument('filepath')
+@click.argument('process')
+@click.argument('directory')
+@click.pass_context
+def start(ctx, filepath, process, directory):
+
+    if not os.path.exists(filepath):
+        print(f"Input file {filepath} doesn't exist")
+         
+    # Figure out how to do some magic to dynamically find a process
+    proc = processes.Multilayer()
+
+    # Establish that the output directory exists by saving a blank conf
+    save_blob(directory, {})
+
+    shutil.copy(filepath, os.path.join(directory, 'input.dxf'))
+    layers = dxfloader.load_layers(filepath)
+    units = proc.layers_to_units(layers)
+    # Make sure we didn't declare any units twice...
+    stage, names = {}, []
+    for i,u in enumerate(units):
+        name = u['name']
+        names.append(name)
+        if name in stage:
+            print(f"Invalid process - unit {name} declared twice")
+            exit(-1)
+        stage[name] = i
+    
+    print("Found workflow units:")
+    for n in names:
+        print(f"    {n}")
+    # Build a json blob containing the state of the current execution flow...
+    blob = {'process' : process, 'units' : units, 'parameters' : {}, 'stage': stage}
+    #... and put it in the output directory
+    print(f"Wrote workflow state to {save_blob(directory, blob)}")
+    
+@main.command()
+@click.argument('directory')
+@click.pass_context
+def list(ctx, directory):
+    blob = os.path.join(directory,"state.json")
+    if not os.path.exists(blob):
+        print(f"Unable to find workflow state {blob}")
+    
+    with open(blob,"r") as f:
+        blob = json.load(f)
+        
+    print("Workflow units:")
+    for u in blob['units']:
+        print(f"    {u['name']}")
+ 
+
+@main.command()
+@click.argument('unit')
+@click.argument('directory')
+@click.pass_context
+def unit(ctx, unit, directory):
+    
+    state = get_blob(directory)
+
+    if unit not in state['stage']:
+        print(f"Can't find unit {unit}")
+        exit(-1)
+
+    # do magic to load the process def from the string in the json...
+    proc = processes.Multilayer()
+    
+        
+    unit_record = state['units'][state['stage'][unit]]
+    
+    # should have optional flags here - reset current parameters, and prompt or just fail
+    ok, new = check_parameters(state, unit)
+
+    if new:
+        save_blob(directory, state)
+    if not ok:
+        print(f"Unspecified parameters for unit {unit}")
+        exit(-1)
+
+    # Load all of the dxf entities we'll need for this unit
+    dxf_entities, errors = dxfloader.load_entities(os.path.join(directory, 'input.dxf'),
+                                                   set().union(*(set(x) for _,x in unit_record['subunits'])))
+    if errors:
+        print("Error loading dxf:")
+        for x in errors:
+            print("    ", x)
+        exit(-1)
+    # Give the newly-reconstituted process a little context as to what's happening
+    proc.parameters = state['parameters']
+    proc.units = state['units']
+
+    
+    with open(os.path.join(directory, unit + ".gcode"),'w') as f:
+        
+        for subname, layers in unit_record['subunits']:
+            full_name = unit, subname
+            gparameters = proc.geometry_parameters(full_name)
+            tol = gparameters['tolerance']
+            del gparameters['tolerance']
+
+            geo = []
+            for layer in layers:
+
+                for entity in dxf_entities[layer]:
+                    geo.append(entity.render_to_tolerance(tol))
+            
+            optimized = pathcleaner.clean_paths(geo,**gparameters)
+            for x in proc.generate_code(full_name, optimized):
+                f.write(x + '\n')
+
+
+        
+    
+if __name__ == '__main__':
+    main(obj = {})
---
+++ dxfloader.py
@@ -0,0 +1,102 @@
+import ezdxf
+import numpy as np
+from collections import defaultdict
+
+class Spline:
+    def __init__(self,degree, control, knots):
+        self.degree = degree
+        self.control = np.array(control)
+        self.knots = np.array(knots)
+        
+    def length_upper_bound(self):
+        acc = 0
+        n, _ = self.control.shape
+        for i in range(1,n):
+            delta = self.control[i] - self.control[i - 1]
+            acc += np.sqrt(delta.dot(delta))
+        
+        return acc
+
+    def render_to_tolerance(self,tolerance):
+        curve = BSpline.Curve()
+        curve.degree = self.degree
+        curve.ctrlpts = self.control.tolist()
+        curve.knotvector = self.knots.tolist()
+        curve.sample_size = max(2, math.ceil(self.length_upper_bound() / tolerance))
+
+        return np.array(curve.evalpts)[:,0:2]
+        
+    @staticmethod
+    def from_dxf(s):
+        return Spline(s.dxf.degree, s.control_points, s.knots)
+    
+class Polyline:
+    def __init__(self, points):
+        self.points = points
+
+    def render_to_tolerance(self, tolerance):
+        # Here for parallelism with Splins - doesn't apply the tolerance
+        return self.points[:,0:2]
+        
+    @staticmethod
+    def from_dxf(line):
+        vertices = [v.dxf.location for v in line.vertices]
+        if line.dxf.flags & 1:
+            vertices.append(vertices[0])
+        return Polyline(np.array(vertices))
+
+def load_layers(fp, remove_empty = True):
+    doc = ezdxf.readfile(fp)
+    layers = [] # something is up with ezdxf, and I can't simply yield the layers as an iterator
+    for l in doc.layers:
+        if l.dxf.plot:
+            layers.append(l.dxf.name)
+
+    if remove_empty:
+
+        unseen = set(layers)
+        for e in doc.modelspace():
+            key = e.dxf.layer
+            if key in unseen:
+                unseen.remove(key)
+            if not unseen:
+                break
+
+        return list(x for x in layers if x not in unseen)
+    
+    return layers
+
+def load_entities(fp, layers):
+    """ Load a subset of a dxf file """
+    msp = ezdxf.readfile(fp).modelspace()
+    objects = defaultdict(lambda: [])
+    errors = []
+
+    splinable = ezdxf.entities.Arc, ezdxf.entities.Circle,  ezdxf.entities.Ellipse
+
+    layers = set(layers)
+    
+    for e in msp:
+        key = e.dxf.layer
+        if key not in layers:
+            continue
+        
+        if isinstance(e, ezdxf.entities.Line):
+            objects[key].append(Polyline(np.array([e.dxf.start, e.dxf.end])))
+        elif isinstance(e,ezdxf.entities.Polyline):
+            if e.dxf.flags > 1:
+                errors.append(("Unsupported polyline flags",e.dxf.handle))
+            else:
+                objects[key].append(Polyline.from_dxf(e))
+                
+        elif isinstance(e, splinable):
+            # If we do this, it magically converts the object to a spline
+            # and appends it to the end of the file (instead of in place),
+            # so we don't have to worry at all!
+            e.to_spline(replace = False)
+        elif isinstance(e, ezdxf.entities.Spline):
+            objects[key].append(Spline.from_dxf(e))
+        else:
+            errors.append((f"Unsupported dxf object - {e}",e.dxf.handle))
+            
+    return dict(objects), errors
